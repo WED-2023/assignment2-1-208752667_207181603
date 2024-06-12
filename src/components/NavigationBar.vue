ad117a2c3042e8b3bee10009689924cacbe2f3b8
Yoav Zelinger <yoavzel@post.bgu.ac.il>
2024-06-12 18:55:26 +0300

    Prototype NavBar

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,95 @@
+<template>
+    <div id="nav">
+      <router-link :to="{ name: 'main' }">Home</router-link>
+      <router-link :to="{ name: 'search' }">Search</router-link>
+      <router-link :to="{ name: 'About' }">About</router-link>
+      <span class="dropdown" v-if="!$root.store.username">
+        <button class="dropdown_button">Hello Guest</button>
+        <div class="dropdown_content">
+          <router-link :to="{ name: 'register' }">Register</router-link>
+          <router-link :to="{ name: 'login' }">Login</router-link>
+        </div>
+      </span>
+      <span class="dropdown" v-else>
+        <button class="dropdown_button">Hello {{ $root.store.username }}</button>
+        <div class="dropdown_content">
+          <button id="logout_button" @click="Logout">Logout</button>
+          <router-link :to="{ name: 'MyFavoriteRecipes' }">My Favorite Recipes</router-link>
+          <router-link :to="{ name: 'MyRecipes' }">My Recipes</router-link>
+          <router-link :to="{ name: 'MyFamilyRecipes' }">My Family Recipes</router-link>
+        </div>
+      </span>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "NavigationBar",
+  methods: {
+    Logout() {
+      this.$root.store.logout();
+      this.$root.toast("Logout", "User logged out successfully", "success");
+      this.$router.push("/").catch(() => {
+        this.$forceUpdate();
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+#nav {
+  background-color: rgb(183, 222, 228);
+	position: fixed;
+	width: 100%;
+	height: 50px;
+	display: flex;
+	font-weight: bold;
+  align-items: center;
+	border-radius: 0px 0px 5px 5px;
+  justify-content: space-around;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+/* Style The Dropdown Button */
+.dropdown_button {
+  font-weight: bold;
+  background-color: rgb(138, 221, 233);;
+  color: white;
+  padding: 15px;
+	border-radius: 0px 0px 5px 5px;
+}
+
+/* The container <div> - needed to position the dropdown content */
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+/* Dropdown Content (Hidden by Default) */
+.dropdown_content {
+  display: none;
+  position: absolute;
+  background-color: rgb(183, 222, 228);
+  min-width: 100%;
+  align-items: center;
+}
+
+/* Links inside the dropdown */
+.dropdown_content a {
+  padding: 12px 16px;
+  display: block;
+}
+
+.dropdown_content a:hover {background-color: #f1f1f1}
+.dropdown_content button:hover {background-color: #f1f1f1}
+.dropdown:hover .dropdown_content {display: block;}
+</style>
--- src/routes.js
+++ src/routes.js
@@ -28,6 +28,31 @@ const routes = [
     component: () => import("./pages/RecipeViewPage"),
   },
   {
+    path: "/about",
+    name: "About",
+    component: () => import("./pages/AboutPage"),
+  },
+  {
+    path: "   ",
+    name: "CreateRecipe",
+    component: () => import("./pages/CreateRecipePage"),
+  },
+  {
+    path: "/myrecipes",
+    name: "MyRecipes",
+    component: () => import("./pages/MyRecipesPage"),
+  },
+  {
+    path: "/familyrecipes",
+    name: "MyFamilyRecipes",
+    component: () => import("./pages/MyFamilyRecipesPage"),
+  },
+  {
+    path: "/favoriterecipes",
+    name: "MyFavoriteRecipes",
+    component: () => import("./pages/MyFavoriteRecipesPage"),
+  },
+  {
     path: "*",
     name: "notFound",
     component: NotFound,
