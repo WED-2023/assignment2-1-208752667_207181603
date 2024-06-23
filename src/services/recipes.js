622e2f0a5f27095cecf2142585fa4da69a2ad32c
Yoav Zelinger <yoavzel@post.bgu.ac.il>
2024-06-23 16:24:13 +0300

    adding search in family recipes in mockGetRecipeFullDetails

--- src/services/recipes.js
+++ src/services/recipes.js
@@ -1,4 +1,5 @@
 import example_recipes from "../assets/mocks/example_recipes.json";
+import family_recipes from "../assets/mocks/family_recipes.json";
 
 function shuffle(array) {
   let currentIndex = array.length;
@@ -41,5 +42,8 @@ export function mockGetLastViewedRecipes(count) {
 export function mockGetRecipeFullDetails(id) {
   id = Number(id);
   let recipe = example_recipes.find(example_recipe => example_recipe.id === id);
+  if (!recipe) {
+    recipe = family_recipes.find(family_recipe => family_recipe.id === id);
+  }
   return { status: 200, response: {data: { recipe: recipe } }};
 }
