ccbd0e9b80a724ec1d538be2a0e66250b58c06ca
Itzak Elbazis <elbazis@post.bgu.ac.il>
2024-07-11 11:35:33 +0300

    dont know why

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
 const state = {
-    // server_domain: "http://localhost:3000",
-    server_domain: "https://yoav-itzik-recipes.cs.bgu.ac.il",
+    server_domain: "http://localhost:3000",
+    // server_domain: "https://yoav-itzik-recipes.cs.bgu.ac.il",
 };
